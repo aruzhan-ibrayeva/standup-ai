c596896a064fec3290f51e0d7d7c4dc23cc74917
aruzhan-ibrayeva <aruzhan.s.ibrayeva@gmail.com>
2024-07-18 23:12:38 +0500

    background

--- frontend/src/components/Scenario.jsx
+++ frontend/src/components/Scenario.jsx
@@ -1,17 +1,26 @@
-import { CameraControls, Environment } from "@react-three/drei";
+import { CameraControls, Environment, useTexture } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { Avatar } from "./Avatar";
 
 export const Scenario = () => {
+    const texture = useTexture("textures/comedy.jpg");
+    const viewport = useThree((state) => state.viewport);
+
     const cameraControls = useRef();
     useEffect(() => {
-        cameraControls.current.setLookAt(0, 2.2, 7, 0, 1.2, 0, true);
+        cameraControls.current.setLookAt(0, 2.5, 10, 0, 2.5, 0, true);
     }, []);
+
     return (
         <>
             <CameraControls ref={cameraControls} />
+            <Avatar position={[0, 1.6, -1]} />
             <Environment preset="sunset" />
-            <Avatar />
+            <mesh position={[0, 2.5, -7]}>
+                <planeGeometry args={[viewport.width*1.7, viewport.height*1.7]} />
+                <meshBasicMaterial map={texture} />
+            </mesh>
         </>
     );
 };
