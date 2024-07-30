7220dc215c6af12ccb18c252497682575e8fe582
aruzhan-ibrayeva <aruzhan.s.ibrayeva@gmail.com>
2024-07-30 15:34:05 +0500

    analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/react"
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -9,7 +8,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <SpeechProvider>
             <App />
-            <Analytics />
         </SpeechProvider>
     </React.StrictMode>
 );
