5c0cc64780b53eda898f4561dac5fb2091235204
aruzhan-ibrayeva <aruzhan.s.ibrayeva@gmail.com>
2024-07-30 17:07:51 +0500

    analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,46 +7,50 @@ import { ChatInterface } from "./components/ChatInterface";
 import ErrorBoundary from "./ErrorBoundary";
 import './App.css';
 import { SpeechProvider } from "./hooks/useSpeech";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
     const [selectedComedian, setSelectedComedian] = useState("DaveChapelle");
 
     return (
-        <SpeechProvider>
-            <div className="app-container flex flex-col md:flex-row">
-                <Loader />
-                <ChatInterface selectedComedian={selectedComedian} />
-                <ErrorBoundary>
-                    <div className="sidebar flex flex-col">
-                        <div className="thumbnail" onClick={() => setSelectedComedian("DaveChapelle")}>
-                            <img src="/davechapelle-thumbnail.jpg" alt="Dave Chapelle" />
-                            <p>Dave Chappelle</p>
+        <>
+            <SpeechProvider>
+                <div className="app-container flex flex-col md:flex-row">
+                    <Loader />
+                    <ChatInterface selectedComedian={selectedComedian} />
+                    <ErrorBoundary>
+                        <div className="sidebar flex flex-col">
+                            <div className="thumbnail" onClick={() => setSelectedComedian("DaveChapelle")}>
+                                <img src="/davechapelle-thumbnail.jpg" alt="Dave Chapelle" />
+                                <p>Dave Chappelle</p>
+                            </div>
+                            <div className="thumbnail" onClick={() => setSelectedComedian("Kharlamov")}>
+                                <img src="/kharlamov-thumbnail.jpg" alt="Kharlamov" />
+                                <p>Garik Kharlamov</p>
+                            </div>
+                            <div className="thumbnail" onClick={() => setSelectedComedian("Saburov")}>
+                                <img src="/saburov-thumbnail.jpg" alt="Saburov" />
+                                <p>Nurlan Saburov</p>
+                            </div>
                         </div>
-                        <div className="thumbnail" onClick={() => setSelectedComedian("Kharlamov")}>
-                            <img src="/kharlamov-thumbnail.jpg" alt="Kharlamov" />
-                            <p>Garik Kharlamov</p>
+                        <div className="canvas-container">
+                            <Canvas
+                                shadows
+                                camera={{ position: [0, 0, -10], fov: 17 }}
+                                onCreated={({ camera }) => {
+                                    camera.lookAt(0, 0, 0);
+                                    camera.updateProjectionMatrix();
+                                }}
+                                style={{ pointerEvents: 'none' }}
+                            >
+                                <Scenario selectedComedian={selectedComedian} />
+                            </Canvas>
                         </div>
-                        <div className="thumbnail" onClick={() => setSelectedComedian("Saburov")}>
-                            <img src="/saburov-thumbnail.jpg" alt="Saburov" />
-                            <p>Nurlan Saburov</p>
-                        </div>
-                    </div>
-                    <div className="canvas-container">
-                        <Canvas
-                            shadows
-                            camera={{ position: [0, 0, -10], fov: 17 }}
-                            onCreated={({ camera }) => {
-                                camera.lookAt(0, 0, 0);
-                                camera.updateProjectionMatrix();
-                            }}
-                            style={{ pointerEvents: 'none' }}
-                        >
-                            <Scenario selectedComedian={selectedComedian} />
-                        </Canvas>
-                    </div>
-                </ErrorBoundary>
-            </div>
-        </SpeechProvider>
+                    </ErrorBoundary>
+                </div>
+            </SpeechProvider>
+            <Analytics />
+        </>
     );
 }
 
